d16f1cdec0713ef2333d7f90b7671f8caf2f85da
Nimai360 <nimai360@hotmail.com>
2023-09-27 18:56:24 -0300

    precisa fazer a verificação de login do cadastro

--- js/login.js
+++ js/login.js
@@ -1,15 +1,67 @@
 function logar() {
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
 
-    if (email.value == "admin@aedo.com" && password.value == "123") {
-        localStorage.setItem("acesso", true);
+  if (email.value == "admin@aedo.com" && password.value == "123") {
+    localStorage.setItem("acesso", true);
 
-        // Evite o envio do formulário padrão
-        event.preventDefault();
+    // Evite o envio do formulário padrão
+    event.preventDefault();
 
-        location.replace("index.html");
-    } else {
-        alert("Usuario ou Senha Invalidos!");
+    location.replace("index.html");
+  } else {
+    alert("Usuario ou Senha Invalidos!");
+  }
+}
+
+function cadastrar() {
+  // event.preventDefault();
+  if (
+    document.getElementById("email").value != "" &&
+    document.getElementById("password").value != "" &&
+    document.getElementById("userCity").value != "" &&
+    document.getElementById("userState").value != "" &&
+    document.getElementById("userAddress").value != "" &&
+    document.getElementById("inputZip").value != "" &&
+    document.getElementById("gridCheck").checked == true
+  ) {
+
+    const user = {
+      email: document.getElementById("email").value,
+      password: document.getElementById("password").value,
+      userCity: document.getElementById("userCity").value,
+      userState: document.getElementById("userState").value,
+      userAddress: document.getElementById("userAddress").value,
+      userAddress2: document.getElementById("userAddress2").value,
+      inputZip: document.getElementById("inputZip").value,
+    };
+
+    if(userExist(user) === false){
+      var users = [];
+      var get = JSON.parse(localStorage.getItem("users"));
+      users = [get];
+      users.push(user);
+
+    localStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("acesso", true);
+    // alert(get2[1].email);
+    } else{
+        alert("Usuário já cadastrado!");
     }
+  }
+
+  function userExist(user) {
+    var get = JSON.parse(localStorage.getItem("users"));
+    array.forEach(element => {
+        if(element )
+    });
+    for (var i = 0; i < get.length; i++) {
+      if (get[i].email === user.email) {
+        alert("Usuário nao cadastrado!");
+        return true;
+      }
+    }
+    alert("Usuário já cadastrado!");
+    return false;
+  }
 }
--- js/redirectPage.js
+++ js/redirectPage.js
@@ -1,20 +1,25 @@
 function menuRedirect(site) {
-  if (site === "about") {
-    window.location.href = "about.html";
-  }
-  if (site === "index") {
-    window.location.href = "index.html";
-  }
-  if (site === "login") {
-    const userLogged = localStorage.getItem("acesso");
-    if (userLogged === "true") {
+  const userLogged = localStorage.getItem("acesso");
+
+  if (userLogged === "true") {
+    if (site === "about") {
+      window.location.href = "about.html";
+    }
+    if (site === "index") {
+      window.location.href = "index.html";
+    }
+    if (site === "login") {
       localStorage.setItem("acesso", "false");
       document.getElementById("login").innerHTML = "Entrar";
       document.getElementById("sigin").removeAttribute("hidden");
+      window.location.href = "login.html";
     }
-    window.location.href = "login.html";
-  }
-  if (site === "formulario") {
-    window.location.href = "formulario.html";
+  } else {
+    if (site === "formulario") {
+      window.location.href = "formulario.html";
+    }
+    if (site === "login") {
+      window.location.href = "login.html";
+    }
   }
 }
