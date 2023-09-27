d565b54e6fac7e341888696850842ba0c41b7d1f
Nimai360 <nimai360@hotmail.com>
2023-09-27 13:31:14 -0300

    adicionado redirecionamento ao realizar o login

--- js/login.js
+++ js/login.js
@@ -5,9 +5,10 @@ function logar() {
     if (email.value == "admin@aedo.com" && password.value == "123") {
         localStorage.setItem("acesso", true);
 
-        alert("Usuario Autenticado.");
+        // Evite o envio do formulário padrão
+        event.preventDefault();
 
-        window.location.href = "index.html";
+        location.replace("index.html");
     } else {
         alert("Usuario ou Senha Invalidos!");
     }
